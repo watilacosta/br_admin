74b78fbd69e6d7de7847f46219ef094255c0e39e
Watila Costa <watila.costa@redealumni.com>
2023-05-02 19:40:20 -0300

    fix render USEROK

--- app/controllers/fsacars_controller.rb
+++ app/controllers/fsacars_controller.rb
@@ -1,18 +1,22 @@
 class FsacarsController < ApplicationController
   before_action :authenticate_pilot!, except: :connect_client
 
-  # http://fsacars.com/demo/userquery.php
   def connect_client
     return render body: 'NOUSR', layout: false unless strong_params[:auth].present?
 
-    sign_in(Pilot.find_by(email: strong_params[:user]))
-
-    return render body: 'NOUSR', layout: false unless current_pilot
-
-    render body: 'USEROK', layout: false
+    render body: sign_in_pilot? ? 'USEROK' : 'NOUSR', layout: false
   end
 
   private
+
+  def sign_in_pilot?
+    pilot = Pilot.find_by!(email: strong_params[:user])
+
+    sign_in(pilot) && pilot.valid_password?(strong_params[:pass])
+  rescue ActiveRecord::RecordNotFound => err
+    Rails.logger.error(err)
+    false
+  end
 
   def strong_params
     params.permit(:auth, :user, :pass, :ver, :lat, :lon, :eqpt)
